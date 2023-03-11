74294d377ffd74ed2d815c5f99114782be9f99dd
xuananle <MY_NAME@example.com>
2023-03-11 22:19:08 +0700

    Refactoring code

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ const double MAIN_OBJECT_SCALE = 0.35;
 const double CHICKEN_OBJECT_SCALE = 1.55;
 const int number_of_asteroid = 20;
 const int chicken_number = 30;
+bool player_want_to_play_again = false;
 const int boss_number = 2;
 bool InitData();
 
@@ -25,7 +26,54 @@ Chicken *chicken2 = new Chicken[chicken_number];
 Boss *boss = new Boss[boss_number];
 Uint32 last_time_present_fall_down = SDL_GetTicks();
 int level = 1;
+int init_system()
+{
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+    {
+        // Handle error
+        std::cerr << "Failed to initialize audio: " << Mix_GetError() << std::endl;
+        // exit or return
+    }
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        std::cerr << "Error initializing SDL: " << SDL_GetError() << std::endl;
+        return 1;
+    }
 
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+    {
+        // Handle error
+        std::cerr << "Failed to initialize audio: " << Mix_GetError() << std::endl;
+        // exit or return
+    }
+    return 0;
+}
+void init_player(MainObject *player)
+{
+    player->load_animation_sprite(renderer, "res/image/ship.png");
+    player->set_clips();
+    player->set_rect_cordinate(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+}
+void init_back_ground(BackGround *back_ground)
+{
+    back_ground->loading_background(renderer, "res/image/background(2).jpg");
+    back_ground->set_speed(1);
+}
+void init_asteroid(Asteroid *asteroid)
+{
+    for (int i = 0; i < number_of_asteroid; i++)
+    {
+        asteroid[i].set_width_height(75, 68);
+        asteroid[i].set_is_on_screen(true);
+        asteroid[i].set_speed(2);
+    }
+}
+void init_random_present(Present *present)
+{
+    present->set_is_on_screen(true);
+    present->set_rect_cordinate(rand() % SCREEN_WIDTH, 0);
+    present->set_kind_of_present(0);
+}
 bool all_level_1_chicken_dead(Chicken *chicken)
 {
     for (int i = 0; i < chicken_number; i++)
@@ -134,7 +182,11 @@ void init_chicken_level_1_2(Chicken *chicken)
         }
     }
 }
-
+void update_game_state()
+{
+    SDL_RenderPresent(renderer);
+    SDL_RenderClear(renderer);
+}
 void process_chicken_vs_player(Chicken *chicken, MainObject *player)
 {
     for (int i = 0; i < chicken_number; i++)
@@ -155,7 +207,6 @@ void process_chicken_vs_player(Chicken *chicken, MainObject *player)
         player->process_if_eat_wing_rect(&chicken[i]);
     }
 }
-
 void player_touch_present(MainObject *player, Present *present)
 {
     Mix_AllocateChannels(100);
@@ -167,75 +218,78 @@ void player_touch_present(MainObject *player, Present *present)
     }
 }
 
-int main(int argc, char *argv[])
+void common_process(MainObject *player, Present *present, SDL_Event &event)
 {
-    srand(time(NULL));
-
-    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+    while (SDL_PollEvent(&event))
     {
-        // Handle error
-        std::cerr << "Failed to initialize audio: " << Mix_GetError() << std::endl;
-        // exit or return
+        if (event.type == SDL_QUIT)
+        {
+            exit(EXIT_SUCCESS);
+        }
+        if (event.type == SDL_KEYDOWN)
+        {
+            if (event.key.keysym.sym == SDLK_ESCAPE)
+            {
+                exit(EXIT_SUCCESS);
+            }
+        }
+        player->handling_movement(event);
+        player->handling_shooting(event);
+    }
+    Uint32 current_time = SDL_GetTicks();
+    if ((current_time - last_time_present_fall_down) >= 10000 && present->get_is_on_screen() == false)
+    {
+        present->set_is_on_screen(true);
+        present->set_rect_cordinate(rand() % SCREEN_WIDTH, 0);
+        present->set_kind_of_present(0);
+        last_time_present_fall_down = current_time;
     }
 
-    init_chicken_level_1(chicken);
-    init_chicken_level_1_2(chicken2);
+    back_ground->render_background_scroll(renderer);
+    back_ground->update_background_scroll();
+    back_ground->set_speed(2);
 
-    present->set_is_on_screen(true);
-    present->set_rect_cordinate(rand() % SCREEN_WIDTH, 0);
-    present->set_kind_of_present(0);
+    present->render();
+    present->update();
 
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-    {
-        std::cerr << "Error initializing SDL: " << SDL_GetError() << std::endl;
-        return 1;
-    }
+    player_touch_present(player, present);
+    player->render_shooting();
+    player->render_animation(renderer, MAIN_OBJECT_SCALE);
+}
 
-    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
-    {
-        // Handle error
-        std::cerr << "Failed to initialize audio: " << Mix_GetError() << std::endl;
-        // exit or return
-    }
-
-    back_ground->loading_background(renderer, "res/image/background(2).jpg");
-    back_ground->set_speed(1);
-
-    player->load_animation_sprite(renderer, "res/image/ship.png");
-    player->set_clips();
-    player->set_rect_cordinate(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
-
-    for (int i = 0; i < number_of_asteroid; i++)
-    {
-        asteroid[i].set_width_height(75, 68);
-        asteroid[i].set_is_on_screen(true);
-        asteroid[i].set_speed(2);
-    }
-
-    // Asteroid *asteroid = new Asteroid();
+void init_boss(Boss *boss)
+{
     for (int i = 0; i < boss_number; i++)
     {
         boss[i].load_animation_sprite(renderer, "res/image/boss.png");
         boss[i].set_clips();
         boss[i].set_rect_cordinate(900, 300);
     }
-
-    Mix_AllocateChannels(100);
-    bool isRunning = true;
-    bool isPause = false;
-    bool player_hit_start = false;
-    Mix_Music *theme_1 = Mix_LoadMUS("res/sound/MENU_THEME.mp3");
-    if (theme_1 == NULL)
-    {
-        std::cout << Mix_GetError();
-        exit(0);
-    }
-    int count = 0;
+}
+void init_menu(GameMenu *menu)
+{
     if (menu->get_texture() == NULL)
     {
         std::cout << "Main.cpp lmao123";
         exit(EXIT_FAILURE);
     }
+}
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    init_menu(menu);
+    init_back_ground(back_ground);
+    init_chicken_level_1(chicken);
+    init_chicken_level_1_2(chicken2);
+    init_random_present(present);
+    init_asteroid(asteroid);
+    init_player(player);
+    init_boss(boss);
+
+    Mix_AllocateChannels(100);
+    Mix_Music *theme_1 = Mix_LoadMUS("res/sound/MENU_THEME.mp3");
+
     Mix_PlayMusic(theme_1, 0);
     while (menu->get_game_has_started() == false)
     {
@@ -253,104 +307,63 @@ int main(int argc, char *argv[])
     }
     theme_1 = Mix_LoadMUS("res/sound/level_1_theme.mp3");
     Mix_PlayMusic(theme_1, 0);
-    while (isRunning)
+    while (level == 1)
     {
-        while (SDL_PollEvent(&event))
+        common_process(player, present, event);
+        process_chicken_vs_player(chicken, player);
+        if (all_level_1_chicken_dead(chicken) == true && all_level_1_2_chicken_dead(chicken2) == true)
         {
-            if (event.type == SDL_QUIT)
-            {
-                isRunning = false;
-            }
-            if (event.type == SDL_KEYDOWN)
-            {
-                if (event.key.keysym.sym == SDLK_ESCAPE)
-                {
-                    isRunning = false;
-                }
-            }
-            player->handling_movement(event);
-            player->handling_shooting(event);
+            level = 2;
         }
-        Uint32 current_time = SDL_GetTicks();
+        else if (all_level_1_chicken_dead(chicken) == true)
+        {
+            process_chicken_vs_player(chicken2, player);
+        }
         if (player->get_health() <= 0)
         {
-            isRunning = false;
+            exit(EXIT_SUCCESS);
         }
-        if ((current_time - last_time_present_fall_down) >= 10000 && present->get_is_on_screen() == false)
-        {
-            present->set_is_on_screen(true);
-            present->set_rect_cordinate(rand() % SCREEN_WIDTH, 0);
-            present->set_kind_of_present(0);
-            last_time_present_fall_down = current_time;
-        }
-
-        back_ground->render_background_scroll(renderer);
-        back_ground->update_background_scroll();
-        back_ground->set_speed(2);
-
-        present->render();
-        present->update();
-
-        if (level == 1)
-        {
-            Mix_ResumeMusic();
-            process_chicken_vs_player(chicken, player);
-            if (all_level_1_chicken_dead(chicken) == true && all_level_1_2_chicken_dead(chicken2) == true)
-            {
-                level = 2;
-            }
-            else if (all_level_1_chicken_dead(chicken) == true)
-            {
-                process_chicken_vs_player(chicken2, player);
-            }
-            if (player->get_health() <= 0)
-            {
-                isRunning = false;
-            }
-        }
-        else if (level == 2)
-        {
-            for (int i = 0; i < number_of_asteroid; i++)
-            {
-                asteroid[i].render_with_angle();
-                asteroid[i].moving_diagonal();
-                asteroid[i].spinning();
-                player->process_if_hit_by_asteroid(&asteroid[i]);
-            }
-            if (all_level_2_asteroid_dead(asteroid) == true)
-            {
-                level = 3;
-            }
-            if (player->get_health() <= 0)
-            {
-                isRunning = false;
-            }
-        }
-        else if (level == 3)
-        {
-            for (int i = 0; i < boss_number; i++)
-            {
-                boss[i].render_animation(renderer, 1);
-                boss[i].moving_toward_player(player);
-                boss[i].firing_eggs(player);
-                boss[i].update_the_eggs();
-                boss[i].render_the_eggs();
-                player->processing_if_hit_by_boss(&boss[i]);
-                player->processing_if_hit_by_boss_egg(&boss[i]);
-            }
-            if (all_boss_dead(boss) == true || player->get_health() <= 0)
-            {
-                isRunning = false;
-            }
-        }
-        player_touch_present(player, present);
-        player->render_shooting();
-        player->render_animation(renderer, MAIN_OBJECT_SCALE);
-
-        SDL_RenderPresent(renderer);
-        SDL_RenderClear(renderer);
+        update_game_state();
     }
-
+    while (level == 2)
+    {
+        common_process(player, present, event);
+        for (int i = 0; i < number_of_asteroid; i++)
+        {
+            asteroid[i].render_with_angle();
+            asteroid[i].moving_diagonal();
+            asteroid[i].spinning();
+            player->process_if_hit_by_asteroid(&asteroid[i]);
+        }
+        if (all_level_2_asteroid_dead(asteroid) == true)
+        {
+            level = 3;
+        }
+        if (player->get_health() <= 0)
+        {
+            exit(EXIT_SUCCESS);
+        }
+        update_game_state();
+    }
+    while (level == 3)
+    {
+        common_process(player, present, event);
+        for (int i = 0; i < boss_number; i++)
+        {
+            boss[i].render_animation(renderer, 1);
+            boss[i].moving_toward_player(player);
+            boss[i].firing_eggs(player);
+            boss[i].update_the_eggs();
+            boss[i].render_the_eggs();
+            player->processing_if_hit_by_boss(&boss[i]);
+            player->processing_if_hit_by_boss_egg(&boss[i]);
+        }
+        if (all_boss_dead(boss) == true || player->get_health() <= 0)
+        {
+            exit(EXIT_SUCCESS);
+        }
+        update_game_state();
+    }
     // FREEING METHOD AND QUITTING
     player->free();
     chicken->free();
